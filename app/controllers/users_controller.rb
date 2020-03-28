70a1276051fc02024b0c766753cc1808537bb05b
Norimitsu-Shimizu <sparesound31@gmail.com>
2020-03-28 18:10:04 +0900

    Decoratorインストール

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find(params[:id]).decorate
     @questions = @user.questions.order("created_at DESC").page(params[:page]).per(10)
   end
   
